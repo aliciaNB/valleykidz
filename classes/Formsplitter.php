69e341a6daaad632492f73b46c559bbb48af1981
Michael Britt <michaelraybritt@gmail.com>
2019-11-06 17:17:18 -0500

    Added more comments to formsplitter to clarivy functions

--- classes/Formsplitter.php
+++ classes/Formsplitter.php
@@ -18,16 +18,18 @@ class Formsplitter
      */
     static function printWeeks($startDate, $endDate, $formNum, $id)
     {
-        $newDate = self::splitDates($startDate,$endDate);
+        $newDate = self::splitDates($startDate,$endDate);//retrive array of monday dates
         $divs ="";
         foreach ($newDate as $dates)
         {
-            $futuredate = new DateTime($dates);
-            $displayStart = $futuredate->format("M d,Y");
-            $futuredate->modify('next saturday');
-            $futuredate->format("Y-m-d");
+            $futuredate = new DateTime($dates);//convert date time object to be converted to next saturday
+            $displayStart = $futuredate->format("M d,Y");//format date for route get hive
+            $futuredate->modify('next saturday');//grab saturday of the weeks monday date provided
+            $futuredate->format("Y-m-d");//format for route get hive
 
-            $displayEnd =$futuredate->format("M d,Y");
+            $displayEnd =$futuredate->format("M d,Y");//format for html example (October 31, 2018)
+
+            //put together href utilizing all the information provided
             $divs = "<li><a href='viewform?form=".$formNum. "&weekStart=".$dates."&weekEnd=".
                 $futuredate->format("Y-m-d")."&id=".$id."'>". $displayStart.
                 " - ". $displayEnd."</a></li>" . $divs;
@@ -45,22 +47,22 @@ class Formsplitter
      */
     private static function splitDates($startDate, $endDate)
     {
-        $startDate = new DateTime($startDate);
-        $endDate = new DateTime($endDate);
-        $dates = [];
+        $startDate = new DateTime($startDate);//convert to date time object
+        $endDate = new DateTime($endDate);//convert to date time object
+        $dates = [];//this will hold all mondays of the week in format Y-m-d
 
-        if($startDate>$endDate)
+        if($startDate>$endDate)//if provided bad values
         {
             return $dates;
         }
-        if(1 !=$startDate->format('N'))
+        if(1 !=$startDate->format('N'))//if start date is not a monday grab previous monday
         {
             $startDate->modify('last monday');
         }
-        while($startDate<=$endDate)
+        while($startDate<=$endDate)//while start date is not past end of form date
         {
-            $dates[]=$startDate->format("Y-m-d");
-            $startDate->modify("+1 week");
+            $dates[]=$startDate->format("Y-m-d"); //add monday date found to list
+            $startDate->modify("+1 week");//increment date by a week to the next monday
         }
         return $dates;
     }
