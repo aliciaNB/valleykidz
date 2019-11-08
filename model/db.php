83ba054d30abf78cc0db23e50155eb10b3d977ea
Alicia <abuehner@mail.greenriver.edu>
2019-11-08 16:25:16 -0500

    Insert comment for default targets statment

--- model/db.php
+++ model/db.php
@@ -138,6 +138,8 @@ UPDATE `clinician` SET `user_name` = 'jelzughbi' WHERE `clinician`.`clinician_id
 INSERT INTO emotions (emotionId,emotionName) VALUES (1,'joy'),(2,'gratitude'),(3,'compassion'),(4,'vulnerability'),
 (5,'self acceptance'),(6,'sadness'),(7,'depression'),(8,'anger'),(9,'frustration'),(10,'anxiety');
 
+INSERT INTO targets (targetName) VALUES ('suicidal ideation'),('self harm'), ('substance use'), ('medication');
+
  */
 /**
  * @author Michael Britt
